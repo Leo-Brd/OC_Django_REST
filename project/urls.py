57430ae67d1fba3fd1603e2775d1a349f12a99ff
Leo-Brd <leobordet.pro@gmail.com>
2025-05-05 11:37:08 +0200

    finish P1C3 exercise

--- project/urls.py
+++ project/urls.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 
 from shop.views import CategoryView
-
+from shop.views import ProductView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/category/', CategoryView.as_view())
+    path('api/category/', CategoryView.as_view()),
+    path('api/product/', ProductView.as_view()),
 ]
